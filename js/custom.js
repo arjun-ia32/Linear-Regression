42ba455fbbedbefd8bc87a1d552fd415a80d24c0
Ramalingeshwar Vedagiri <octo.nebula@gmail.com>
2020-08-09 22:23:35 +0530

    Code cleanup - use table library and separate out stat code

--- js/custom.js
+++ js/custom.js
@@ -1,182 +1,5 @@
 "use strict";
 
-Array.prototype.sum = function(op_func) {
-    op_func = op_func || ((x) => x);
-    return this.reduce((acc, x) => acc + op_func(x), 0);
-}
-
-/*
-    For summing up multiple arrays. Callback
-    function must be the last argument passed,
-    preceded by arrays to sum.
-
-    Cardinality of all arrays must be equal.
-
-    -Rama
-*/
-function sum(...args) {
-    let i, j;
-    let s = 0;
-
-    let op_func = args[args.length - 1];
-    for (i = 0; i < args[0].length; ++i) {
-        let op_args = [];
-        for (j = 0; j < args.length - 1; ++j) {
-            op_args.push(args[j][i]);
-        }
-        s += op_func(...op_args);
-    }
-
-    return s;
-}
-
-// Convert seconds to hh:mm string
-function seconds_to_string(secs) {
-    let hrs = Math.trunc(secs / 3600);
-    let mins = Math.trunc(secs / 60) - hrs * 60;
-    if (hrs < 10)
-        hrs = `0${hrs}`;
-    if (mins < 10)
-        mins = `0${mins}`;
-    return `${hrs}:${mins}`;
-}
-
-function create_interval_data_table(step) {
-    if (step % 900 != 0) {
-        throw "Step must in 15 minute intervals";
-    }
-
-    let intervals = [];
-    for (let i = 0; i < 86400; i += step) {
-        let blank_row = { interval: seconds_to_string(i), mon: 0, tue: 0, wed: 0, thu: 0, fri: 0, sat: 0, sun: 0 };
-        intervals.push(blank_row);
-    }
-
-    return intervals;
-}
-
-// Arithmatic mean
-function mean(X) {
-    return X.sum() / X.length;
-}
-
-// Variance of population
-function variance_p(X) {
-    let m = mean(X);
-    return X.sum(x => (x - m) ** 2) / X.length;
-}
-
-// Variance of sample
-function variance_s(X) {
-    let m = mean(X);
-    return X.sum(x => (x - m) ** 2) / (X.length - 1);
-}
-
-// Standard deviation of population
-function stddev_p(X) {
-    return Math.sqrt(variance_p(X));
-}
-
-// Standard deviation of sample
-function stddev_s(X) {
-    return Math.sqrt(variance_s(X));
-}
-
-function check_cardinality(X, Y) {
-    if (X.length != Y.length) {
-        throw "Both Xs must have the same cardinality!";
-    }
-}
-
-// Covariance of population
-function covariance_p(X, Y) {
-    check_cardinality(X, Y);
-    let mx = mean(X);
-    let my = mean(Y);
-    return sum(X, Y, (x, y) => (x - mx) * (y - my)) / X.length;
-}
-
-// Covariance of sample
-function covariance_s(X, Y) {
-    check_cardinality(X, X);
-    let mx = mean(X);
-    let my = mean(Y);
-    return sum(X, Y, (x, y) => (x - mx) * (y - my)) / (X.length - 1);
-}
-
-// Correlation coefficient of population
-function correlation_coeff_p(X, Y) {
-    check_cardinality(X, Y);
-    return covariance_p(X, Y) / (stddev_p(X) * stddev_p(Y));
-}
-
-// Correlation coefficient of sample
-function correlation_coeff_s(X, Y) {
-    check_cardinality(X, Y);
-    return covariance_s(X, Y) / (stddev_p(X) * stddev_p(Y));
-}
-
-// Coefficient of determination of population
-function coeff_of_determination_p(X, Y) {
-    return correlation_coeff_p(X, Y) ** 2;
-}
-
-// Coefficient of determination of sample
-function coeff_of_determination_s(X, Y) {
-    return correlation_coeff_s(X, Y) ** 2;
-}
-
-// Pearson product moment correlation coefficient
-function ppmcc(X, Y) {
-    check_cardinality(X, Y);
-
-    let mx = mean(X), my = mean(Y);
-    let sum_xy = 0, sum_x2 = 0, sum_y2 = 0;
-    let x, y;
-    for (let i = 0; i < X.length; ++i) {
-        x = X[i] - mx;
-        y = Y[i] - my;
-        sum_xy += x * y;
-        sum_x2 += x * x;
-        sum_y2 += y * y;
-    }
-
-    return sum_xy / Math.sqrt(sum_x2 * sum_y2);
-}
-
-/*
-    Simple linear regression by least squares method
-    https://stattrek.com/regression/regression-example.aspx?tutorial=reg
-
-    -Rama
-*/
-function linear_regression_b1(X, Y) {
-    check_cardinality(X, Y);
-    let meanx = mean(X), meany = mean(Y);
-    return sum(X, Y, (x, y) => (x - meanx) * (y - meany)) / X.sum(x => (x - meanx) ** 2);
-}
-
-function linear_regression_b0(X, Y) {
-    return mean(Y) - linear_regression_b1(X, Y) * mean(X);
-}
-
-function linear_regression_predict(X, Y, new_x) {
-    return linear_regression_b0(X, Y) + linear_regression_b1(X, Y) * new_x;
-}
-
-function linear_regression_apply(X, Y) {
-    let b0 = linear_regression_b0(X, Y);
-    let b1 = linear_regression_b1(X, Y);
-
-    return X.map(x => b0 + b1 * x);
-}
-
-function standard_error_slope(X, E) {
-    check_cardinality(X, E);
-    let mx = mean(X);
-    return Math.sqrt(E.sum(e => e ** 2) / (E.length - 2)) / Math.sqrt(X.sum(x => (x - mx) ** 2));
-}
-
 /*
     Misc helper functions
 
@@ -195,12 +18,7 @@ function create_data_table(X, Y, YY) {
     return table;
 }
 
-/*
-    Chart functions
-
-    -Rama
-*/
-const getCircularReplacer = () => {
+const get_circular_replacer = () => {
     const seen = new WeakSet();
     return (key, value) => {
       if (typeof value === "object" && value !== null) {
@@ -212,6 +30,12 @@ const getCircularReplacer = () => {
       return value;
     };
 };
+
+/*
+    Chart functions
+
+    -Rama
+*/
 
 const scatter_join_points_plugin = {
     afterDatasetsDraw: function (chart, easing) {
@@ -287,6 +111,24 @@ function create_residual_plot_data(data_table) {
     };
 }
 
+function create_normal_distribution_data(data_table) {
+    let dataset1 = data_table.map(x => x.y);
+    //let dataset1 = data_table;
+    console.log(dataset1);
+    let colour1 = "rgba(0, 0, 255, 0.65)";
+    return {
+        labels: data_table.map(x => x.x),
+        datasets: [{
+            label: 'Normal Distribution',
+            borderColor: colour1,
+            backgroundColor: colour1, //Chart.helpers.color(colour).alpha(0.2).rgbString(),
+            fill:false,
+            //pointStyle: 'rect',
+            data: dataset1
+        }]
+    };
+}
+
 function create_scatter_chart(data_table, canvas_id) {
     let chart_data_table = data_table.values;
     window.chart_data = create_scatter_chart_data(chart_data_table);
@@ -321,297 +163,18 @@ function create_residual_plot(data_table, canvas_id) {
 }
 
 
-/*
-    Some functions for HTML tables
+function create_normal_distribution_chart(data_table, canvas_id) {
+    window.normal_distribution_data = create_normal_distribution_data(data_table.values);
 
-    -Rama
-*/
-
-function html_table_on_paste_default_handler(table, td) {
-    return function(event) {
-        let data = (event.clipboardData || window.clipboardData).getData("text");
-        data = data.slice(0, -1);
-        let row_data = data.split("\n");
-        let row_start;
-        let new_td = td;
-
-        if (row_data.length <= 0)
-            return;
-
-        console.log(row_data);
-        let max_rows = Math.min(table.rows.length - new_td.parentElement.rowIndex, row_data.length);
-        for (let i = 0; i < max_rows; ++i) {
-            let column_data = row_data[i].split("\t");
-            let columns = new_td.parentElement.cells.length;
-            let paste_columns = column_data.length;
-            let max_columns = Math.min(columns - new_td.cellIndex, paste_columns);
-
-            for (let j = 0; j < max_columns; ++j) {
-                new_td.parentElement.cells[j + new_td.cellIndex].innerText = column_data[j];
-            }
-
-            if (new_td.parentElement.rowIndex < table.rows.length - 1)
-                new_td = table.rows[new_td.parentElement.rowIndex + 1].cells[new_td.cellIndex];
-        }
-
-        console.log(table.rows.length);
-        event.preventDefault();
-    };
-}
-
-function create_html_table(table_data, settings) {
-    let table = document.createElement("table");
-    let thead = document.createElement("thead");
-    let tr = document.createElement("tr");
-
-    // Bootstrap classes
-    table.classList.add("table-striped");
-    table.classList.add("table-bordered");
-    if (settings.classes && settings.classes.table) {
-        settings.classes.table.forEach((i) => {
-            table.classList.add(i);
-        });
-    }
-    table.id = settings.table_id;
-    table.internal_data = table_data;
-    table.internal_settings = settings;
-
-    for (var i in table_data.values[0]) {
-        if (table_data.values[0].hasOwnProperty(i)) {
-            let th = document.createElement("th");
-            if (settings.classes && settings.classes.th) {
-                settings.classes.th.forEach((i) => {
-                    th.classList.add(i);
-                });
-            }
-            th.style.textAlign = "center";
-            th.style.verticalAlign = "center";
-            th.innerHTML = table_data.headers[i];
-            tr.appendChild(th);
-        }
-    }
-
-    if (settings.enable_insert_remove_gui) {
-        // Add extra column for row insertion/removal control buttons
-        let th = document.createElement("th");
-        th.classList.add("px-1");
-        th.style.textAlign = "center";
-        th.style.verticalAlign = "center";
-        th.innerHTML = "+-";
-        tr.appendChild(th);
-    }
-
-    thead.appendChild(tr);
-    table.appendChild(thead);
-
-    let tbody = document.createElement("tbody");
-    for (let i = 0; i < table_data.values.length; ++i) {
-        tr = document.createElement("tr");
-        for (var j in table_data.values[0]) {
-            if (table_data.values[0].hasOwnProperty(j)) {
-                let td = document.createElement("td");
-                let value = table_data.values[i][j];
-                td.style.textAlign = "center";
-                td.contentEditable = true;
-                td.innerText = value;
-                if (!isNaN(value) && !Number.isInteger(value) && settings.round_threshold) {
-                    td.innerText = value.toFixed(settings.round_threshold);
-                }
-                td.internal_i = i;
-                td.internal_j = j;
-                if (settings.table_on_change_callback)
-                    td.addEventListener("input", settings.table_on_change_callback(table, td));
-                if (settings.table_on_paste_callback)
-                    td.addEventListener("paste", settings.table_on_paste_callback(table, td));
-                else
-                    td.addEventListener("paste", html_table_on_paste_default_handler(table, td));
-                tr.appendChild(td);
+    let ctx = document.getElementById(canvas_id).getContext("2d");
+    window.normal_distribution_chart = new Chart(ctx, {
+        type: "line",
+        data:  window.normal_distribution_data,
+        options: {
+            title: {
+                display: false,
+                text: "Normal Distribution"
             }
         }
-
-        if (settings.enable_insert_remove_gui) {
-            // Add button for row insertion/removal control
-            let td = document.createElement("td");
-            td.style.textAlign = "center";
-            let pair = create_table_insert_remove_buttons();
-
-            pair.insert.addEventListener("click", table_handle_insert_row(table, tr, settings.table_on_change_callback));
-            pair.remove.addEventListener("click", table_handle_remove_row(table, tr, settings.table_on_change_callback));
-            td.appendChild(pair.insert);
-            td.appendChild(pair.remove);
-            tr.appendChild(td);
-        }
-
-        tbody.appendChild(tr);
-    }
-
-    table.appendChild(tbody);
-    return table;
-}
-
-function create_table_insert_remove_buttons() {
-    let button_insert = document.createElement("button");
-    let button_remove = document.createElement("button");
-    button_insert.style.width = button_remove.style.width = "30px";
-    button_insert.innerText = "+";
-    button_remove.innerText = "-";
-    button_insert.classList.add("btn");
-    button_insert.classList.add("btn-primary");
-    button_insert.classList.add("btn-sm");
-    //button_insert.classList.add("px-1");
-    //button_insert.classList.add("py-1");
-    button_insert.classList.add("ml-1");
-    button_remove.classList.add("btn");
-    button_remove.classList.add("btn-danger");
-    button_remove.classList.add("btn-sm");
-    //button_remove.classList.add("px-0");
-    //button_remove.classList.add("py-1");
-    button_remove.classList.add("mx-1");
-    //button_remove.classList.add("my-1");
-
-    return { insert: button_insert, remove: button_remove };
-}
-
-function table_handle_insert_row(table, tr, table_on_change_callback) {
-    return function() {
-        let new_tr = table.insertRow(tr.rowIndex+1);
-        let empty_content = {};
-        for (var i in table.internal_data.values[0]) {
-            if (table.internal_data.values[0].hasOwnProperty(i)) {
-                empty_content[i] = 0;
-                let td = document.createElement("td");
-                td.style.textAlign = "center";
-                td.contentEditable = true;
-                td.innerText = 0;
-                td.internal_i = new_tr.rowIndex;
-                td.internal_j = i;
-                td.addEventListener("input", table_on_change_callback(table, td));
-                new_tr.appendChild(td);
-            }
-        }
-        table.internal_data.values.splice(tr.rowIndex, 0, empty_content);
-
-        // Add button for row insertion/removal control
-        let td = document.createElement("td");
-        td.style.textAlign = "center";
-        let pair = create_table_insert_remove_buttons();
-        pair.insert.addEventListener("click", table_handle_insert_row(table, new_tr, table_on_change_callback));
-        pair.remove.addEventListener("click", table_handle_remove_row(table, new_tr, table_on_change_callback));
-        td.appendChild(pair.insert);
-        td.appendChild(pair.remove);
-        new_tr.appendChild(td);
-
-        console.log("inserting over");
-        console.log(table);
-    };
-}
-
-function update_table(table) {
-    if (table.internal_data.values.length <= 0)
-        return;
-
-    for (let i = 1; i < table.rows.length; ++i) {
-        Object.keys(table.internal_data.values[i - 1]).forEach((key, idx) => {
-            let value = table.internal_data.values[i - 1][key];
-            table.rows[i].cells.item(idx).innerText = value;
-            //console.log("Updating " + key + " at idx " + idx + " with value " + value);
-            if (!isNaN(value) && !Number.isInteger(value) && table.internal_settings.round_threshold) {
-                table.rows[i].cells.item(idx).innerText = value.toFixed(table.internal_settings.round_threshold);
-            }
-        });
-    }
-}
-
-function table_handle_remove_row(table, tr) {
-    return function() {
-        table.internal_data.values.splice(tr.rowIndex - 1, 1);
-        //console.log("deleting " + tr.rowIndex - 1);
-        //console.log(table.internal_data.values);
-        table.deleteRow(tr.rowIndex);
-        update_table(table);
-        update_charts(table);
-    };
-}
-
-function get_data_from_html_table(html_table) {
-    let thead = html_table.getElementsByTagName("thead")[0];
-    let tbody = html_table.getElementsByTagName("tbody")[0];
-    let data_table = [];
-
-    let tr = thead.getElementsByTagName("tr")[0];
-
-    let headers = [];
-    for (let i = 0; i < tr.children.length; ++i) {
-        headers.push(tr.children[i].innerText);
-    }
-
-    for (let i = 0; i < tbody.children.length; ++i) {
-        let tr = tbody.children[i];
-        data_table[i] = {};
-        for (let j = 0; j < tr.children.length; ++j) {
-            data_table[i][headers[j]] = tr.children[j].innerText;
-        }
-    }
-
-    return data_table;
-}
-
-/*
-    Test functions ahead - just for internal testing and/or benchmarking
-
-    -Rama
-*/
-
-// Pearson product moment correlation coefficient
-// Uses reduce instead of our custom sum - slightly faster than ppmcc3
-function ppmcc2(X, Y) {
-    check_cardinality(X, Y);
-    let meanx = mean(X);
-    let meany = mean(Y);
-    let x = X.map(x => x - meanx);
-    let y = Y.map(y => y - meany);
-
-    let sum_xy = x.reduce((acc, x, i) => acc + x * y[i], 0);
-    let sum_x2 = x.sum(x => x * x);
-    let sum_y2 = y.sum(y => y * y);
-    return sum_xy / Math.sqrt(sum_x2 * sum_y2);
-}
-
-// Loop version of Pearson peroduct moment correlation coefficient
-// Uses our custom version of sum - is slowest in benchmarks
-function ppmcc3(X, Y) {
-    check_cardinality(X, Y);
-    let meanx = mean(X);
-    let meany = mean(Y);
-    let x = X.map(x => x - meanx);
-    let y = Y.map(y => y - meany);
-
-    let sum_xy = sum(x, y, (x, y) => x * y);
-    let sum_x2 = x.sum(x => x * x);
-    let sum_y2 = y.sum(y => y * y);
-    return sum_xy / Math.sqrt(sum_x2 * sum_y2);
-}
-
-function test_ppmcc_speed(X, Y) {
-    let i, times = 100000;
-    console.time("ppmcc1");
-    for (i = 0; i < times; ++i)
-        ppmcc(X, Y);
-    console.timeEnd("ppmcc1");
-
-    console.time("ppmcc2");
-    for (i = 0; i < times; ++i)
-        ppmcc2(X, Y);
-    console.timeEnd("ppmcc2");
-
-    console.time("ppmcc3");
-    for (i = 0; i < times; ++i)
-        ppmcc3(X, Y);
-    console.timeEnd("ppmcc3");
-
-    let p1 = ppmcc(X, Y);
-    let p2 = ppmcc2(X, Y);
-    let p3 = ppmcc3(X, Y);
-
-    console.log("ppmcc1 is " + p1 + "; ppmcc2 is " + p2 + "; ppmcc3 is " + p3);
+    });
 }
---
+++ js/stat_functions.js
@@ -0,0 +1,248 @@
+"use strict";
+
+Array.prototype.sum = function(op_func) {
+    op_func = op_func || ((x) => x);
+    return this.reduce((acc, x) => acc + op_func(x), 0);
+}
+
+/*
+    For summing up multiple arrays. Callback
+    function must be the last argument passed,
+    preceded by arrays to sum.
+
+    Cardinality of all arrays must be equal.
+
+    -Rama
+*/
+function sum(...args) {
+    let i, j;
+    let s = 0;
+
+    let op_func = args[args.length - 1];
+    for (i = 0; i < args[0].length; ++i) {
+        let op_args = [];
+        for (j = 0; j < args.length - 1; ++j) {
+            op_args.push(args[j][i]);
+        }
+        s += op_func(...op_args);
+    }
+
+    return s;
+}
+
+// Convert seconds to hh:mm string
+function seconds_to_string(secs) {
+    let hrs = Math.trunc(secs / 3600);
+    let mins = Math.trunc(secs / 60) - hrs * 60;
+    if (hrs < 10)
+        hrs = `0${hrs}`;
+    if (mins < 10)
+        mins = `0${mins}`;
+    return `${hrs}:${mins}`;
+}
+
+function create_interval_data_table(step) {
+    if (step % 900 != 0) {
+        throw "Step must in 15 minute intervals";
+    }
+
+    let intervals = [];
+    for (let i = 0; i < 86400; i += step) {
+        let blank_row = { interval: seconds_to_string(i), mon: 0, tue: 0, wed: 0, thu: 0, fri: 0, sat: 0, sun: 0 };
+        intervals.push(blank_row);
+    }
+
+    return intervals;
+}
+
+// Arithmatic mean
+function mean(X) {
+    return X.sum() / X.length;
+}
+
+// Variance of population
+function variance_p(X) {
+    let m = mean(X);
+    return X.sum(x => (x - m) ** 2) / X.length;
+}
+
+// Variance of sample
+function variance_s(X) {
+    let m = mean(X);
+    return X.sum(x => (x - m) ** 2) / (X.length - 1);
+}
+
+// Standard deviation of population
+function stddev_p(X) {
+    return Math.sqrt(variance_p(X));
+}
+
+// Standard deviation of sample
+function stddev_s(X) {
+    return Math.sqrt(variance_s(X));
+}
+
+function check_cardinality(X, Y) {
+    if (X.length != Y.length) {
+        throw "Both Xs must have the same cardinality!";
+    }
+}
+
+// Covariance of population
+function covariance_p(X, Y) {
+    check_cardinality(X, Y);
+    let mx = mean(X);
+    let my = mean(Y);
+    return sum(X, Y, (x, y) => (x - mx) * (y - my)) / X.length;
+}
+
+// Covariance of sample
+function covariance_s(X, Y) {
+    check_cardinality(X, X);
+    let mx = mean(X);
+    let my = mean(Y);
+    return sum(X, Y, (x, y) => (x - mx) * (y - my)) / (X.length - 1);
+}
+
+// Correlation coefficient of population
+function correlation_coeff_p(X, Y) {
+    check_cardinality(X, Y);
+    return covariance_p(X, Y) / (stddev_p(X) * stddev_p(Y));
+}
+
+// Correlation coefficient of sample
+function correlation_coeff_s(X, Y) {
+    check_cardinality(X, Y);
+    return covariance_s(X, Y) / (stddev_p(X) * stddev_p(Y));
+}
+
+// Coefficient of determination of population
+function coeff_of_determination_p(X, Y) {
+    return correlation_coeff_p(X, Y) ** 2;
+}
+
+// Coefficient of determination of sample
+function coeff_of_determination_s(X, Y) {
+    return correlation_coeff_s(X, Y) ** 2;
+}
+
+// Pearson product moment correlation coefficient
+function ppmcc(X, Y) {
+    check_cardinality(X, Y);
+
+    let mx = mean(X), my = mean(Y);
+    let sum_xy = 0, sum_x2 = 0, sum_y2 = 0;
+    let x, y;
+    for (let i = 0; i < X.length; ++i) {
+        x = X[i] - mx;
+        y = Y[i] - my;
+        sum_xy += x * y;
+        sum_x2 += x * x;
+        sum_y2 += y * y;
+    }
+
+    return sum_xy / Math.sqrt(sum_x2 * sum_y2);
+}
+
+/*
+    Simple linear regression by least squares method
+    https://stattrek.com/regression/regression-example.aspx?tutorial=reg
+
+    -Rama
+*/
+function linear_regression_b1(X, Y) {
+    check_cardinality(X, Y);
+    let meanx = mean(X), meany = mean(Y);
+    return sum(X, Y, (x, y) => (x - meanx) * (y - meany)) / X.sum(x => (x - meanx) ** 2);
+}
+
+function linear_regression_b0(X, Y) {
+    return mean(Y) - linear_regression_b1(X, Y) * mean(X);
+}
+
+function linear_regression_predict(X, Y, new_x) {
+    return linear_regression_b0(X, Y) + linear_regression_b1(X, Y) * new_x;
+}
+
+function linear_regression_apply(X, Y) {
+    let b0 = linear_regression_b0(X, Y);
+    let b1 = linear_regression_b1(X, Y);
+
+    return X.map(x => b0 + b1 * x);
+}
+
+function standard_error_slope(X, E) {
+    check_cardinality(X, E);
+    let mx = mean(X);
+    return Math.sqrt(E.sum(e => e ** 2) / (E.length - 2)) / Math.sqrt(X.sum(x => (x - mx) ** 2));
+}
+
+function normal_distribution(X) {
+    let s = stddev_p(X);
+    let mul = Math.sqrt(2 * Math.PI) / s;
+    let mx = mean(X);
+    let var2 = 2 * (s ** 2);
+
+   return X.map((x) => mul * Math.pow(Math.E, -((x - mx) ** 2) / var2));
+}
+
+
+/*
+    Test functions ahead - just for internal testing and/or benchmarking
+
+    -Rama
+*/
+
+// Pearson product moment correlation coefficient
+// Uses reduce instead of our custom sum - slightly faster than ppmcc3
+function ppmcc2(X, Y) {
+    check_cardinality(X, Y);
+    let meanx = mean(X);
+    let meany = mean(Y);
+    let x = X.map(x => x - meanx);
+    let y = Y.map(y => y - meany);
+
+    let sum_xy = x.reduce((acc, x, i) => acc + x * y[i], 0);
+    let sum_x2 = x.sum(x => x * x);
+    let sum_y2 = y.sum(y => y * y);
+    return sum_xy / Math.sqrt(sum_x2 * sum_y2);
+}
+
+// Loop version of Pearson peroduct moment correlation coefficient
+// Uses our custom version of sum - is slowest in benchmarks
+function ppmcc3(X, Y) {
+    check_cardinality(X, Y);
+    let meanx = mean(X);
+    let meany = mean(Y);
+    let x = X.map(x => x - meanx);
+    let y = Y.map(y => y - meany);
+
+    let sum_xy = sum(x, y, (x, y) => x * y);
+    let sum_x2 = x.sum(x => x * x);
+    let sum_y2 = y.sum(y => y * y);
+    return sum_xy / Math.sqrt(sum_x2 * sum_y2);
+}
+
+function test_ppmcc_speed(X, Y) {
+    let i, times = 100000;
+    console.time("ppmcc1");
+    for (i = 0; i < times; ++i)
+        ppmcc(X, Y);
+    console.timeEnd("ppmcc1");
+
+    console.time("ppmcc2");
+    for (i = 0; i < times; ++i)
+        ppmcc2(X, Y);
+    console.timeEnd("ppmcc2");
+
+    console.time("ppmcc3");
+    for (i = 0; i < times; ++i)
+        ppmcc3(X, Y);
+    console.timeEnd("ppmcc3");
+
+    let p1 = ppmcc(X, Y);
+    let p2 = ppmcc2(X, Y);
+    let p3 = ppmcc3(X, Y);
+
+    console.log("ppmcc1 is " + p1 + "; ppmcc2 is " + p2 + "; ppmcc3 is " + p3);
+}
